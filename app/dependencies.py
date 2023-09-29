aa549adc86a5b6244e2a8035b48b8ca1fea64d90
lcasan <lcasan120100@gmai.com>
2023-09-29 14:48:58 +0200

    Description added to files

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,8 +1,20 @@
 from models import Order, Criterion
 
 def process_orders(orders: list[Order], criterion: Criterion):
+    
+    '''
+        This function filter the orders based on the criterion 
+        and return the total revenue for the filtered orders.
+    '''
+
     total = 0.0
-    for order in orders:
-        if order.status == criterion:
+
+    if criterion != "all":
+        for order in orders:
+            if order.status == criterion:
+                total += (order.quantity * order.price)
+    else:
+        for order in orders:
             total += (order.quantity * order.price)
+    
     return total
--- app/models.py
+++ app/models.py
@@ -7,13 +7,37 @@ Criterion = Literal[Status, "all"]
 
 
 class Order(BaseModel):
-    id: int
-    item: str = Field(..., min_length=1)
-    quantity: int = Field(..., ge=0)
-    price: float = Field(..., ge=0)
-    status: Status
+    id: int = Field(
+        ...,
+        description = "An integer representing the order ID",
+        ge=0
+    )
+    item: str = Field(
+        ...,
+        description = "A string representing the item name", 
+        min_length=1
+    )
+    quantity: int = Field(
+        ...,
+        description = "An integer representing the number of items in the order", 
+        gt=0
+    )
+    price: float = Field(
+        ...,
+        description = "A number representing the price per item",
+        ge=0
+    )
+    status: Status = Field(
+        description = "A string representing the order status"
+    )
 
 
 class Json(BaseModel):
-    orders: list[Order]
-    criterion: Criterion
+    orders: list[Order] = Field(
+        ...,
+        description = "List of orders"
+    )
+    criterion: Criterion = Field(
+        ...,
+        description = "A string that indicates the filter to be applied to the orders"
+    )
