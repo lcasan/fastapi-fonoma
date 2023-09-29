b78093b75e579992851c5b8f68abe8ac07e20b4e
lcasan <lcasan120100@gmai.com>
2023-09-30 01:53:18 +0200

    Added type annotations

--- app/main.py
+++ app/main.py
@@ -11,6 +11,15 @@ app = FastAPI()
 
 @app.post("/solution")
 def solution(msg: MSG) -> float:
+    """
+    The endpoint return the result of executing the function process_orders with the parameters
+
+    Parameters:
+    - msg: A ModelBase containing the orders and the criterion.
+
+    Returns:
+    - float: The total revenue for the filtered orders.
+    """
     result = cache.get(msg.model_dump_json())
     
     if not result:
